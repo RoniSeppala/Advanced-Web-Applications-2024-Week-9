4586ca08b765fb1fbd3f15a3fb50e3bdb459c87e
Roni Seppälä <roni.o.seppala@gmail.com>
2025-01-20 10:09:07 +0200

    refactor MyContainer and MyList components to use structured item props and improve list rendering

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-interface MyListProps {
-    listItems: string[];
+type TItem = {
+    id: string;
+    text: string;
+};
+
+interface ListProps {
+    header:string;
+    items:TItem[];
 }
 
-const MyList:React.FC<MyListProps> = ({listItems}) => {
+const MyList:React.FC<ListProps> = ({header,items}) => {
     return (
         <div>
-        <h1>My List</h1>
-        <h2> { listItems } </h2>
+            <h1>{header}</h1>
+            <ol>
+                {items.map((item) => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ol>
         </div>
     );
 }
